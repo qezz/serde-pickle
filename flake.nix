25d56c848c2cba3492668718bf96be036a957538
Sergey Mishin <sergei.a.mishin@gmail.com>
2025-11-24 21:58:33 +0100

    nix: add flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "serde-pickle dev env";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-mozilla.url = "github:mozilla/nixpkgs-mozilla";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      nixpkgs-mozilla,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import nixpkgs-mozilla) ];
+        };
+
+        rusttoolchain =
+          (pkgs.rustChannelOf {
+            rustToolchain = ./rust-toolchain.toml;
+            sha256 = "sha256-KXx+ID0y4mg2B3LHp7IyaiMrdexF6octADnAtFIOjrY=";
+          }).rust;
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = [ pkgs.openssl ];
+            nativeBuildInputs = with pkgs; [
+              rusttoolchain
+              pkg-config
+              python313
+            ];
+
+            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+          };
+        };
+      }
+    );
+}
